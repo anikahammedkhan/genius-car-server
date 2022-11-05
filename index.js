a22d2f7b61bf246d62c2321f79cb6838910b909c
Md Anik Ahammed Khan <anikkhan1105@gmail.com>
2022-11-05 09:52:58 +0600

    basic node mongo setup

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4kqreoc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Genius server is running');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
